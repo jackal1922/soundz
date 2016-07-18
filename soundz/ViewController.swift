42e05d13eca3122f41d53ae2895666f32817288b
Jackal Pan <jackal1922@gmail.com>
2016-07-19 00:03:01 +0800

    Initial Commit

---
+++ soundz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soundz
+//
+//  Created by 潘則諺 on 2016/7/19.
+//  Copyright © 2016年 Jackal Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
